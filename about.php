b2e395c848be2ca84c5ad05827cd720ad12a90bd
manikandanarunachalam <manikandanarunachalam913@gmail.com>
2024-11-26 15:40:21 +0530

    New Design Update 26/11/24

---
+++ about.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CCM About Page</title>
+</head>
+<body>
+<?php include './HeaderFooter/header.php'; ?>
+    <section class="banner">
+    
+    <div class="container">
+  <div class="row text-center">
+    <h1 class="col-12">About CCM Awards</h1>
+
+    <div class="col-md-12 d-flex justify-content-center">
+      <div class="coin p-6">
+        <img src="./images/CCM LOGO 3d2.png" class="img-fluid" alt="Coin Image" style="width: 400px; height: 400px;">
+      </div>
+      
+    </div>
+    <div class="row text-center">
+
+    <h3 class="col-12">More Than Just Awards.</h3>
+    <p>
+Founded in 2021, the CapeComorin Video then 2022 CapeComorin Digital Awards is a prestigious event that holds opportunity to bring together the most brilliant, talented and active Tamilnadu content creators across various fields and multiple platforms, in aim to celebrate their remarkable achievements.
+
+CapeComorin Digital Awards is a thrilling experience to showcase these immacble talents, and offer them the recognition and support they deserve for their entertaining, inspirational and educating content.</p>
+  </div>
+    </div>
+    
+</div>
+
+
+    
+</section>
+<section class="banner">
+    
+    <div class="container">
+  <div class="row text-center">
+    <h1 class="col-12">About CCM Team</h1>
+
+    <div class="col-md-12 d-flex justify-content-center">
+      <div class="coin p-6">
+        <img src="./images/ccm-team.jpeg" class="img-fluid" alt="Coin Image" style="width: 800px; height: 500px;">
+      </div>
+      
+    </div>
+    <p>hhuikjhukjg</p>
+  </div>
+</div>
+
+
+    
+</section>
+
+<!-- home Banner Section End -->
+
+<!-- Registration Box Section and Dummy About Section -->
+<section class="banner2">
+<div class="card">
+
+<div class="col-12 col-md-12 gradient-box d-flex flex-column justify-content-between align-items-center ">
+
+	
+	
+	
+
+<p>Start Your Self / Favourite Nomination</p>
+    <a href="Register.php" class="glass-button  text-decoration-none text-white">Submit Your Nomination</a>
+    
+</div>
+
+
+            </div>
+      
+
+    
+
+    </div>
+</section>
+<?php include "./HeaderFooter/footer.php" ?>
+</body>
+</html>
---
+++ archive.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CCM Archive Page</title>
+</head>
+<body>
+    <?php include "./HeaderFooter/header.php" ?>
+    <section class="banner">
+    
+    <div class="container">
+  <div class="row text-center">
+    <h1 class="col-12">Archive CCM Awards</h1>
+
+    <div class="col-md-12 d-flex justify-content-center">
+      <div class="coin p-6">
+        <img src="./images/CCM LOGO 3d2.png" class="img-fluid" alt="Coin Image" style="width: 400px; height: 400px;">
+      </div>
+      
+    </div>
+  </div>
+</div>
+
+
+    
+</section>
+<section class="banner">
+    
+    <div class="container">
+  <div class="row text-center">
+    <h1 class="col-12">About CCM Awards</h1>
+
+    <div class="col-md-12 d-flex justify-content-center">
+      <div class="coin p-6">
+        <img src="./images/ccmteam.JPG" class="img-fluid" alt="Coin Image" style="width: 700px; height: 400px;">
+      </div>
+      
+    </div>
+  </div>
+</div>
+
+
+    
+</section>
+
+<!-- home Banner Section End -->
+
+<!-- Registration Box Section and Dummy About Section -->
+<section class="banner2">
+<div class="card">
+
+<div class="col-12 col-md-12 gradient-box d-flex flex-column justify-content-between align-items-center ">
+
+	
+	
+	
+
+<p>Creator Registeration</p>
+    <a href="Register.php" class="glass-button  text-decoration-none text-white">Creator  Registration</a>
+    
+</div>
+
+
+            </div>
+      
+
+    
+
+    </div>
+</section>
+    <?php include "./HeaderFooter/footer.php" ?>
+</body>
+</html>
