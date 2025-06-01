137c84ed52b342b240fa5494133ec3eb56d5b56a
manikandanarunachalam <127881832+Amanikandan1997@users.noreply.github.com>
2025-06-01 12:45:12 +0530

    Update archive.php

--- archive.php
+++ archive.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CCM Archive Page</title>
+    <title>CCM Team /title>
 </head>
 <body>
     <?php include "./HeaderFooter/header.php" ?>
